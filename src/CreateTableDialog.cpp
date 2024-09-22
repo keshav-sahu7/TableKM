a5be1f64864724f41069be66c7f1e2d730a7bada
Keshav sahu <keshavpiproud999@gmail.com>
2024-09-22 22:59:47 +0530

    fix: remove unnecessary icons.

--- src/CreateTableDialog.cpp
+++ src/CreateTableDialog.cpp
@@ -14,10 +14,8 @@
 #include <kmt/ErrorHandler.hpp>
 
 #include "KPairedLineEdit.hpp"
-#include "SyntaxHighlighter.hpp"
 #include "ThemeHandler2.hpp"
 #include "CustomErrMsgBox.hpp"
-#include "misc/ValidatorFactory.hpp"
 #include "misc/KDefines.h"
 
 
